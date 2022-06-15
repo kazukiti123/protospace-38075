5aa0d17488f1dcf96b303ec8bb16649e74de9da1
kazukiti123 <106233096+kazukiti123@users.noreply.github.com>
2022-06-15 18:05:47 +0900

    最後の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
 def create
   @comment = Comment.new(comment_params)
-  @comment.save
   if @comment.save 
     redirect_to prototype_path(@comment.prototype.id)
  else 
