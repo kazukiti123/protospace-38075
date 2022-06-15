4eab5d5580693c147c9eb074d5e1b82ce16b9c19
kazukiti123 <106233096+kazukiti123@users.noreply.github.com>
2022-06-15 20:08:50 +0900

    routingの調整

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,12 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only:[:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only:[:new, :edit, :destroy]
+
   def index
     @prototypes = Prototype.all
   end
 
   def new
-    @prototype = Prototype.new
+    @prototype=Prototype.new
   end
 
   def create
@@ -18,37 +18,38 @@ class PrototypesController < ApplicationController
     end
   end
 
-  def show
-    @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
-  end
+ def show
+  @prototype = Prototype.find(params[:id])
+  @comment = Comment.new
+  @comments = @prototype.comments.includes(:user)
+ end
 
-  def edit
-    user = @prototype.user
-    unless user == current_user
-      redirect_to action: :index
-    end
+ def edit
+  @prototype = Prototype.find(params[:id])
+  user = @prototype.user
+  unless user == current_user
+    redirect_to action: :index
+    
   end
+ end
 
-  def update
-    if @prototype.update(prototype_params)
-      redirect_to prototype_path
-    else
-      render :edit
-    end
-  end
+ def update
+  @prototype = Prototype.find(params[:id])
+  if @prototype.update(prototype_params)
+    redirect_to prototype_path
+  else 
+    render :edit
+ end
+end
 
-  def destroy
-    @prototype.destroy
-    redirect_to root_path
-  end
+def destroy
+  @prototype =  Prototype.find(params[:id])
+  @prototype.destroy
+  redirect_to root_path
+end
 
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
  root to: "prototypes#index"
  resources :prototypes do
-   resources :comments
+   resources :comments, only: :create
  end
 resources :users,only: :show
 end
